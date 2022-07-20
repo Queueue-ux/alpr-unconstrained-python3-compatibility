0e6553ac5d707aeab1dfb2dd5a750f6901877118
quinton <quintoncarlisle@gmail.com>
2022-07-19 22:10:04 -0600

    wrong import

--- src/keras_utils.py
+++ src/keras_utils.py
@@ -5,9 +5,9 @@ import time
 
 from os.path import splitext
 
-from src.label import Label
-from src.utils import getWH, nms
-from src.projection_utils import getRectPts, find_T_matrix
+from .. src.label import Label
+from .. src.utils import getWH, nms
+from .. src.projection_utils import getRectPts, find_T_matrix
 
 
 class DLabel (Label):
@@ -24,17 +24,17 @@ def save_model(model,path,verbose=0):
 	with open('%s.json' % path,'w') as json_file:
 		json_file.write(model_json)
 	model.save_weights('%s.h5' % path)
-	if verbose: print 'Saved to %s' % path
+	if verbose: print('Saved to %s' % path)
 
 def load_model(path,custom_objects={},verbose=0):
-	from tensorflow.keras.utils import model_from_json
+	from tensorflow.keras.models import model_from_json
 
 	path = splitext(path)[0]
 	with open('%s.json' % path,'r') as json_file:
 		model_json = json_file.read()
 	model = model_from_json(model_json, custom_objects=custom_objects)
 	model.load_weights('%s.h5' % path)
-	if verbose: print 'Loaded from %s' % path
+	if verbose: print('Loaded from %s' % path)
 	return model
 
 
